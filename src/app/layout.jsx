ee571968afeebfe47e81a8cf580159a244e3993d
G3RGES <Georgios.Nashaat@gmail.com>
2023-11-15 10:28:34 +0200

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,10 @@
 import { Alex_Brush, Montserrat } from "next/font/google";
 import "./globals.css";
 
+// components
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
 const alexBrush = Alex_Brush({
   weight: ["400"],
   subsets: ["latin"],
@@ -23,7 +27,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${alexBrush.variable} ${montserrat.variable} overflow-x-hidden relative`}
       >
+        <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
