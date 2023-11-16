ff8e06224029dd934629b3d7dea89518490d273f
G3RGES <Georgios.Nashaat@gmail.com>
2023-11-16 09:45:17 +0200

    Update Events.jsx

--- src/components/events/Events.jsx
+++ src/components/events/Events.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import EventBox from "./EventBox";
+import SectionHeader from "./SectionHeader";
 
 const getEvents = async () => {
   const res = await fetch("http://localhost:4000/events");
@@ -12,6 +13,7 @@ const Events = async () => {
   return (
     <section className="section" id="tours">
       <div className="container mx-auto">
+        <SectionHeader pretitle="World Tour" title="Upcoming Events" />
         {/* event box */}
         <EventBox events={events} />
       </div>
