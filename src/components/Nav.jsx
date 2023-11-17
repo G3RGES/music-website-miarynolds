45165aecce59eda5381995bd5e142908b3fcc1ae
G3RGES <Georgios.Nashaat@gmail.com>
2023-11-18 00:10:29 +0200

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import { Link } from "react-scroll";
+
+const Links = [
+  {
+    path: "home",
+    name: "Home",
+  },
+  {
+    path: "tours",
+    name: "Tours",
+  },
+  {
+    path: "discography",
+    name: "Discography",
+  },
+  {
+    path: "contact",
+    name: "Contact",
+  },
+];
 
 const Nav = ({ containerStyles, linkStyles }) => {
-  return <nav className="hidden xl:flex">Nav</nav>;
+  return (
+    <nav className={`${containerStyles}`}>
+      {Links.map((link, idx) => {
+        return (
+          <Link
+            key={idx}
+            to={link.path}
+            className={`${linkStyles} cursor-pointer border-b-2 border-transparent`}
+            smooth
+            spy
+            offset={-50}
+            activeClass="active"
+          >
+            {link.name}
+          </Link>
+        );
+      })}
+    </nav>
+  );
 };
 
 export default Nav;
