edbdaf43dc5841c60eca0699b1044af38aa1d6c3
G3RGES <Georgios.Nashaat@gmail.com>
2023-11-17 11:28:38 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,67 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+import Image from "next/image";
+import Link from "next/link";
+
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
+
+// components
+import NavMobile from "./NavMobile";
 
 const Header = () => {
-  return <header className="fixed z-50 w-full ">Header</header>;
+  const [active, setActive] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      // detect scroll
+      setActive(window.scrollY > 100);
+    };
+
+    // add event listener
+    window.addEventListener("scroll", handleScroll);
+
+    // clear event listener
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <header
+      className={`fixed z-50 w-full ${
+        active ? "bg-[#030315] py-6" : "bg-transparent py-8"
+      }`}
+    >
+      <div
+        className="container mx-auto flex flex-col 
+      xl:flex-row items-center justify-between"
+      >
+        {/* logo */}
+        <Link
+          href={"/"}
+          className="relative flex w-[225px] h-[37.64px] transition-all mb-4 xl:mb-0 "
+        >
+          <Image
+            src={"/assets/header/logo.svg"}
+            fill
+            alt="header_logo"
+            className="object-contain"
+          />
+        </Link>
+        {/* nav */}
+        <nav>nav</nav>
+        {/* nav mobile */}
+        <NavMobile />
+        {/* menu btn */}
+        <div>menu btn</div>
+        {/* socials */}
+        <div>social icons</div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
