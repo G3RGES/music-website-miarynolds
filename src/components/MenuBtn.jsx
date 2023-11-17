0e936095e33df9c7774f27904b4200a58b2c285c
G3RGES <Georgios.Nashaat@gmail.com>
2023-11-18 00:59:17 +0200

    Update MenuBtn.jsx

--- src/components/MenuBtn.jsx
+++ src/components/MenuBtn.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const MenuBtn = () => {
   return (
-    <div className="group flex flex-col gap-y-2 cursor-pointer xl:hidden">
+    <div className="group flex flex-col gap-y-2 items-end cursor-pointer transition-all xl:hidden">
       <div className="w-7 h-[2px] bg-white"></div>
-      <div className="w-7 h-[2px] bg-white"></div>
+      <div className="w-4 group-hover:w-7 h-[2px] bg-white"></div>
       <div className="w-7 h-[2px] bg-white"></div>
     </div>
   );
