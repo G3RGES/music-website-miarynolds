d149e0eca57691a44b9d50fbff9d80fed2f5d3b4
G3RGES <Georgios.Nashaat@gmail.com>
2023-11-18 23:44:59 +0200

    Update AlbumSlider.jsx

--- src/components/albums/AlbumSlider.jsx
+++ src/components/albums/AlbumSlider.jsx
@@ -1,6 +1,14 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
+
+import useSWR from "swr";
+import Image from "next/image";
+
+import { AudioPlayer } from "react-audio-play";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCoverflow, FreeMode, Navigation, Thumbs } from "swiper/modules";
 
 const AlbumSlider = () => {
   return <div>AlbumSlider</div>;
