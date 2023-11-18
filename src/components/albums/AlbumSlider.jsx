4f31304064050d02f5ef75fb9dd0586e4d397bdf
G3RGES <Georgios.Nashaat@gmail.com>
2023-11-19 00:19:41 +0200

    Update AlbumSlider.jsx

--- src/components/albums/AlbumSlider.jsx
+++ src/components/albums/AlbumSlider.jsx
@@ -14,7 +14,14 @@ import "swiper/css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 
+// fetcher
+const fetcher = (url) => {
+  fetch(url).then((res) => res.json());
+};
+
 const AlbumSlider = () => {
+  const { data, error } = useSWR("http://localhost:4000/albums", fetcher);
+
   return <div>AlbumSlider</div>;
 };
 
