eec0df60c000ec6b3285973b2114ce88280c8a7f
G3RGES <Georgios.Nashaat@gmail.com>
2023-11-18 09:58:41 +0200

    Update MenuBtn.jsx

--- src/components/MenuBtn.jsx
+++ src/components/MenuBtn.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import { NavContext } from "@/context/NavContext";
+import React, { useContext } from "react";
 
 const MenuBtn = () => {
+  const { setIsOpen } = useContext(NavContext);
+
   return (
-    <div className="group flex flex-col gap-y-2 items-end cursor-pointer transition-all xl:hidden">
+    <div
+      onClick={() => setIsOpen(true)}
+      className="group flex flex-col gap-y-2 items-end cursor-pointer transition-all xl:hidden"
+    >
       <div className="w-7 h-[2px] bg-white"></div>
       <div className="w-4 group-hover:w-7 h-[2px] bg-white"></div>
       <div className="w-7 h-[2px] bg-white"></div>
