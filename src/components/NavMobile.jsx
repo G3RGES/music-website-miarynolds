2effaa2fa0f39109e097c9579e82fdc740a8304e
G3RGES <Georgios.Nashaat@gmail.com>
2023-11-18 09:58:47 +0200

    Update NavMobile.jsx

--- src/components/NavMobile.jsx
+++ src/components/NavMobile.jsx
@@ -1,6 +1,12 @@
 import { NavContext } from "@/context/NavContext";
 import React, { useContext } from "react";
 
+// icons
+import { RiCloseLine } from "react-icons/ri";
+
+// components
+import Nav from "./Nav";
+
 const NavMobile = () => {
   const { isOpen, setIsOpen } = useContext(NavContext);
 
@@ -8,10 +14,19 @@ const NavMobile = () => {
     <nav
       className={`
       ${isOpen ? "right-0" : "-right-full"}
-         xl:hidden fixed bg-red-500 w-full top-0 z-20 bottom-0 transition-all
+         xl:hidden fixed bg-primary w-full top-0 z-20 bottom-0 transition-all
          duration-500`}
     >
-      NavMobile
+      <div className="absolute right-4 top-5">
+        <RiCloseLine
+          onClick={() => setIsOpen(false)}
+          className="text-5xl cursor-pointer"
+        />
+      </div>
+      <Nav
+        containerStyles="flex flex-col items-center justify-center
+       gap-y-8 text-3xl uppercase font-bold h-full"
+      />
     </nav>
   );
 };
