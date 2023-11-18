be0d35cedd84bdaeef48bd552197ed661a0f8443
G3RGES <Georgios.Nashaat@gmail.com>
2023-11-18 06:59:14 +0200

    Update Socials.jsx

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
+import Link from "next/link";
+
+import {
+  RiYoutubeFill,
+  RiInstagramFill,
+  RiSpotifyFill,
+  RiSoundcloudFill,
+} from "react-icons/ri";
+
 const Socials = () => {
   return <div>Socials</div>;
 };
