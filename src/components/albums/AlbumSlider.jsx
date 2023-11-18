ccc47f0e2830de83774678d130a17e6618063577
G3RGES <Georgios.Nashaat@gmail.com>
2023-11-19 01:15:09 +0200

    Update AlbumSlider.jsx

--- src/components/albums/AlbumSlider.jsx
+++ src/components/albums/AlbumSlider.jsx
@@ -15,14 +15,45 @@ import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 
 // fetcher
-const fetcher = (url) => {
-  fetch(url).then((res) => res.json());
-};
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
 const AlbumSlider = () => {
   const { data, error } = useSWR("http://localhost:4000/albums", fetcher);
+  console.log(data);
 
-  return <div>AlbumSlider</div>;
+  if (error) return "Failed to fetch data";
+  if (!data) return "Loading...";
+
+  return (
+    <>
+      {/* top slider */}
+      <Swiper>
+        {data.map((album) => {
+          // console.log(album); //Testing
+          return (
+            <SwiperSlide key={album.id} className="mb-12">
+              <div>
+                {/* image */}
+                <div className="hidden xl:flex w-[300px] h-[300px] xl:w-[500px] xl:h-[500px] ">
+                  <Image
+                    src={album.img}
+                    alt="album_img"
+                    fill
+                    priority
+                    className="object-contain"
+                  />
+                </div>
+                {/* track container */}
+                <div>track container</div>
+              </div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+      {/* thumb slider */}
+      <Swiper>thumb slider</Swiper>
+    </>
+  );
 };
 
 export default AlbumSlider;
