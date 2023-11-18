b32923c0d1f09a0fb010654d522e23338db52f90
G3RGES <Georgios.Nashaat@gmail.com>
2023-11-18 23:35:28 +0200

    Update Albums.jsx

--- src/components/albums/Albums.jsx
+++ src/components/albums/Albums.jsx
@@ -2,8 +2,21 @@
 
 import React from "react";
 
+import AlbumSlider from "./AlbumSlider";
+
+import { motion } from "framer-motion";
+import { fadeIn } from "../../../variants";
+
+import SectionHeader from "../events/SectionHeader";
+
 const Albums = () => {
-  return <div>Albums</div>;
+  return (
+    <section id="discography">
+      <div className="container mx-auto">
+        <SectionHeader pretitle="Discography" title="Popular Albums" />
+      </div>
+    </section>
+  );
 };
 
 export default Albums;
