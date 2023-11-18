6f0d96e45f624d028a34710c10fd154fd41cbaf7
G3RGES <Georgios.Nashaat@gmail.com>
2023-11-18 08:55:03 +0200

    Update NavMobile.jsx

--- src/components/NavMobile.jsx
+++ src/components/NavMobile.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { NavContext } from "@/context/NavContext";
+import React, { useContext } from "react";
 
 const NavMobile = () => {
-  return <nav className="xl:hidden bg-red-500">NavMobile</nav>;
+  const { isOpen, setIsOpen } = useContext(NavContext);
+
+  return (
+    <nav
+      className={`
+      ${isOpen ? "right-0" : "-right-full"}
+         xl:hidden fixed bg-red-500 w-full top-0 z-20 bottom-0 transition-all
+         duration-500`}
+    >
+      NavMobile
+    </nav>
+  );
 };
 
 export default NavMobile;
