0f499dd43ea4de2be1bcd31fbce899b485d7239e
G3RGES <Georgios.Nashaat@gmail.com>
2023-11-19 16:12:05 +0200

    Update Blog.jsx

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+import SectionHeader from "../events/SectionHeader";
+import PostList from "./PostList";
+
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <section className="section" id="blog">
+      Blog
+    </section>
+  );
 };
 
 export default Blog;
