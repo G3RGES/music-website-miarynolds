47ee3464b90430f9f39636c2f8b7b91892ac896b
G3RGES <Georgios.Nashaat@gmail.com>
2023-11-19 16:06:18 +0200

    Update Albums.jsx

--- src/components/albums/Albums.jsx
+++ src/components/albums/Albums.jsx
@@ -15,9 +15,14 @@ const Albums = () => {
       <div className="container mx-auto">
         <SectionHeader pretitle="Discography" title="Popular Albums" />
         {/* album slider */}
-        <div>
+        <motion.div
+          variants={fadeIn("up", 0.2)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.3 }}
+        >
           <AlbumSlider />
-        </div>
+        </motion.div>
       </div>
     </section>
   );
