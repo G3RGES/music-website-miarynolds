6445044aefb9d0f11364309102d99d813eb4285a
G3RGES <Georgios.Nashaat@gmail.com>
2023-11-20 06:22:21 +0200

    Update Newsletter.jsx

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -8,7 +8,30 @@ import { motion } from "framer-motion";
 import { fadeIn } from "../../variants";
 
 const Newsletter = () => {
-  return <section className="bg-newsletter">Newsletter</section>;
+  return (
+    <section
+      className="bg-newsletter bg-fixed h-[480px] section 
+    bg-cover w-full bg-center bg-no-repeat"
+      id="contact"
+    >
+      <div className="container mx-auto h-full">
+        <div>
+          {/* text */}
+          <SectionHeader
+            pretitle="Get in Touch"
+            title="Sign up to our newsletter"
+          />
+          {/* input */}
+          <div className="relative flex items-center w-full max-w-xl  ">
+            <input type="text" placeholder="Email address" className="" />
+            <button type="submit" className="bg-pink-400">
+              Subscripe
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Newsletter;
