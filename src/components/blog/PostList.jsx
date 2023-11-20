b1b92ca20401ddabdf9e39966b038635cd064ec6
G3RGES <Georgios.Nashaat@gmail.com>
2023-11-20 04:38:38 +0200

    Update PostList.jsx

--- src/components/blog/PostList.jsx
+++ src/components/blog/PostList.jsx
@@ -1,6 +1,16 @@
+"use client";
+
 import React from "react";
 
-const PostList = () => {
+import Link from "next/link";
+
+import { BsArrowRight } from "react-icons/bs";
+
+import { motion } from "framer-motion";
+import { fadeIn } from "../../../variants";
+
+const PostList = ({ posts }) => {
+  // console.log(posts); //testing
   return <div>PostList</div>;
 };
 
