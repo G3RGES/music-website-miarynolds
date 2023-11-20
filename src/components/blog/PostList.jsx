d6a9f483ccb35c46729a66bd80ec44982527cb94
G3RGES <Georgios.Nashaat@gmail.com>
2023-11-20 05:26:41 +0200

    Update PostList.jsx

--- src/components/blog/PostList.jsx
+++ src/components/blog/PostList.jsx
@@ -16,7 +16,40 @@ const PostList = ({ posts }) => {
   const firstThreePosts = posts.slice(0, 3);
   // console.log(firstThreePosts); //testing
 
-  return <div>PostList</div>;
+  return (
+    <motion.div
+      variants={fadeIn("up", 0.4)}
+      initial="hidden"
+      whileInView={"show"}
+      viewport={{ once: false, amount: 0.3 }}
+      className="flex flex-col items-center "
+    >
+      <div
+        className="flex flex-col xl:flex-row justify-between gap-12 py-10 xl:pt-16 
+      xl:pb-24 border-t border-white/10  "
+      >
+        {firstThreePosts.map((post) => {
+          // distructure post
+          const { id, date, title, description } = post;
+          return (
+            <div key={id} className="flex-1">
+              {/* date */}
+              <div className="text-accent font-bold mb-1  ">{date}</div>
+              {/* title */}
+              <div className="text-xl font-medium mb-4 ">{title}</div>
+              {/* description */}
+              <p className="text-white/30 mb-6 font-light">{description}</p>
+              <Link href="#" className="flex items-center gap-x-2 group">
+                Read More
+                <BsArrowRight className="text-accent text-xl group-hover:ml-1 transition-all" />
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+      <button className="btn btn-lg btn-accent">View All Posts</button>
+    </motion.div>
+  );
 };
 
 export default PostList;
