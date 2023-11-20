c46025be13d7566cf321539151cd2eccf34691f1
G3RGES <Georgios.Nashaat@gmail.com>
2023-11-20 04:26:57 +0200

    Update Blog.jsx

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -3,10 +3,20 @@ import React from "react";
 import SectionHeader from "../events/SectionHeader";
 import PostList from "./PostList";
 
-const Blog = () => {
+const getPosts = async () => {
+  const res = await fetch("http://localhost:4000/posts");
+  return res.json();
+};
+
+const Blog = async () => {
+  const posts = await getPosts();
+  // console.log(posts); // testing
+
   return (
     <section className="section" id="blog">
-      Blog
+      <div className="container mx-auto">
+        <SectionHeader pretitle="Our Blog" title="Latest News" />
+      </div>
     </section>
   );
 };
